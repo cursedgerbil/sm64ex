095061648e6d2ab12ad81da1286cbb0fae9d7315
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2025-08-08 13:34:12 +0100

    Implement cap timer

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -929,6 +929,7 @@ void update_hud_values(void) {
         gHudDisplay.stars = gMarioState->numStars;
         gHudDisplay.lives = gMarioState->numLives;
         gHudDisplay.keys = gMarioState->numKeys;
+        gHudDisplay.captimer = gMarioState->capTimer / 60;
 
         if (numHealthWedges > gHudDisplay.wedges) {
             play_sound(SOUND_MENU_POWER_METER, gDefaultSoundArgs);
