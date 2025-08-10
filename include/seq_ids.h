37ce6f4439993f2012948d6344d1d13ac0dc3a36
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2025-08-10 20:29:55 +0100

    Update seq_ids.h

--- include/seq_ids.h
+++ include/seq_ids.h
@@ -41,7 +41,7 @@ enum SeqId {
     SEQ_EVENT_CUTSCENE_LAKITU,        // 0x22 (not in JP)
     SEQ_TICKTOCK,                     // 0x23
     SEQ_RAINBOW,                      // 0x24
-    SEQ_EVENT_OWL,                      // 0x25
+    SEQ_EVENT_OWL,                    // 0x25
     SEQ_COUNT
 };
 
