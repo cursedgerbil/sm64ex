222e6dbb617c91c9702e3a09f3c40c6cc66a592a
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2025-08-10 17:09:18 +0100

    Update macro.inc.c

--- levels/bob/areas/1/macro.inc.c
+++ levels/bob/areas/1/macro.inc.c
@@ -27,6 +27,8 @@ const MacroObject bob_seg7_macro_objs[] = {
     MACRO_OBJECT(/*preset*/ macro_bobomb,                    /*yaw*/   0, /*pos*/  6888,  2002, -5608),
     MACRO_OBJECT(/*preset*/ macro_recovery_heart,            /*yaw*/   0, /*pos*/  3603,  3659, -7070),
     MACRO_OBJECT(/*preset*/ macro_coin_ring_horizontal,      /*yaw*/   0, /*pos*/ -6060,  1060, -5340),
+    MACRO_OBJECT(/*preset*/ macro_coin_ring_horizontal,      /*yaw*/   0, /*pos*/ -4300,  1060,  2400),
+    MACRO_OBJECT(/*preset*/ macro_coin_ring_horizontal,      /*yaw*/   0, /*pos*/  6400,   970,  1500),
     MACRO_OBJECT(/*preset*/ macro_coin_line_horizontal,      /*yaw*/  90, /*pos*/  -129,     0,  5668),
     MACRO_OBJECT(/*preset*/ macro_coin_line_horizontal,      /*yaw*/  75, /*pos*/  1851,  2917, -2234),
     MACRO_OBJECT(/*preset*/ macro_red_coin,                  /*yaw*/   0, /*pos*/ -5500,   768,  2400),
