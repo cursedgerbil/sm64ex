5792661add5271c0efdec009ba35893169ca88e0
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2025-08-11 20:50:45 +0100

    Update capswitch.inc.c

--- src/game/behaviors/capswitch.inc.c
+++ src/game/behaviors/capswitch.inc.c
@@ -1,4 +1,5 @@
 // capswitch.c.inc
+#include "sm64ap.h"
 
 void cap_switch_act_0(void) {
     o->oAnimState = o->oBehParams2ndByte;
@@ -6,7 +7,7 @@ void cap_switch_act_0(void) {
     o->oPosY += 71.0f;
     spawn_object_relative_with_scale(0, 0, -71, 0, 0.5f, o, MODEL_CAP_SWITCH_BASE, bhvCapSwitchBase);
     if (gCurrLevelNum != LEVEL_UNKNOWN_32) {
-        if (save_file_get_flags() & D_8032F0C0[o->oBehParams2ndByte]) {
+        if (SM64AP_PressedSwitch(D_8032F0C0[o->oBehParams2ndByte])) {
             o->oAction = 3;
             o->header.gfx.scale[1] = 0.1f;
         } else
