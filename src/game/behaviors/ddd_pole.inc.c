1a1aab0266e2033deb8fd94472530439351c332f
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2025-08-11 20:54:02 +0100

    Update ddd_pole.inc.c

--- src/game/behaviors/ddd_pole.inc.c
+++ src/game/behaviors/ddd_pole.inc.c
@@ -1,22 +1,27 @@
+#include "../../sm64ap.h"
 
 void bhv_ddd_pole_init(void) {
-    if (!(save_file_get_flags() & (SAVE_FLAG_HAVE_KEY_2 | SAVE_FLAG_UNLOCKED_UPSTAIRS_DOOR))) {
-        obj_mark_for_deletion(o);
-    } else {
-        o->hitboxDownOffset = 100.0f;
-        o->oDDDPoleMaxOffset = 100.0f * o->oBehParams2ndByte;
-    }
+    o->oHomeY = o->oPosY;
+    o->hitboxDownOffset = 100.0f;
+    o->oDDDPoleMaxOffset = 100.0f * o->oBehParams2ndByte;
 }
 
 void bhv_ddd_pole_update(void) {
-    if (o->oTimer > 20) {
-        o->oDDDPoleOffset += o->oDDDPoleVel;
+    if (SM64AP_CheckedLoc(SM64AP_ID_KEY2) && SM64AP_CheckedLoc(SM64AP_LOCATIONID_BOARDBOWSERSSUB)) {
+        if (o->oPosY != o->oHomeY) {
+            o->oPosY = o->oHomeY;
+        }
+        if (o->oTimer > 20) {
+            o->oDDDPoleOffset += o->oDDDPoleVel;
 
         if (clamp_f32(&o->oDDDPoleOffset, 0.0f, o->oDDDPoleMaxOffset)) {
-            o->oDDDPoleVel = -o->oDDDPoleVel;
-            o->oTimer = 0;
-        }
+                o->oDDDPoleVel = -o->oDDDPoleVel;
+                o->oTimer = 0;
+            }
     }
 
-    obj_set_dist_from_home(o->oDDDPoleOffset);
+        obj_set_dist_from_home(o->oDDDPoleOffset);
+    } else {
+        o->oPosY = -10000.0f;
+    }
 }
