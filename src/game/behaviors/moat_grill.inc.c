d06b7709934bbb9eb38c4ade76f8ccc641f2aa8d
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2025-08-11 20:57:18 +0100

    Update moat_grill.inc.c

--- src/game/behaviors/moat_grill.inc.c
+++ src/game/behaviors/moat_grill.inc.c
@@ -1,7 +1,7 @@
 // moat_grill.c.inc
 
 void bhv_moat_grills_loop(void) {
-    if (save_file_get_flags() & SAVE_FLAG_MOAT_DRAINED)
+    if (SM64AP_MoatDrained())
         cur_obj_set_model(MODEL_NONE);
     else
         load_object_collision_model();
