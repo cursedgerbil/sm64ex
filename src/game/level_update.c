b9758d89246e3f6efab4338745022d945709d515
cursedgerbil <62775523+cursedgerbil@users.noreply.github.com>
2026-04-05 19:12:09 +0100

    Update level_update.c

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -736,7 +736,7 @@ void initiate_painting_warp(void) {
 
 
                 if (!(warpNode.destLevel & 0x80)) {
-                    D_8032C9E0 = check_warp_checkpoint(&warpNode);
+                    D_8032C9E0 = FALSE;
                 }
 
                 initiate_warp(warpNode.destLevel & 0x7F, warpNode.destArea, warpNode.destNode, 0);
